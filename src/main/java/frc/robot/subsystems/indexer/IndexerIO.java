c6d844d4a846c0867e5d3382e79fcae12aa3d67c
Will E <wre136@hotmail.com>
2026-02-14 13:16:45 -0500

    Got rid of excess imports

--- src/main/java/frc/robot/subsystems/indexer/IndexerIO.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIO.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.indexer;
 
-import org.littletonrobotics.junction.AutoLog;
-
 public interface IndexerIO {
     // public default void setIndexerPower(double power){} 
     // public default void setIndexWallPower(double power){}
