2acf86b2d718586889c179d9b75d0961f57ae20a
Will E <wre136@hotmail.com>
2026-02-15 13:41:55 -0500

    Updated Kicke Subsystem classes to have improved code and be properly documented

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,16 +1,7 @@
 package frc.robot.subsystems.shooter;
+
 import org.littletonrobotics.junction.Logger;
-
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.indexer.IndexerIO;
-import frc.robot.subsystems.indexer.IndexerIOInputs;
-import frc.robot.subsystems.indexer.IndexerIOTalonFX;
 
 public class Shooter extends SubsystemBase{
     private double power = 0;
