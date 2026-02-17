cc5cfb9329de9ef00c11dddb795339a96ba15b96
FRC6366RamRodz <atrmorphis@gmail.com>
2026-02-17 18:36:17 -0500

    why not

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026_Mummy";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 49;
+  public static final String GIT_SHA = "3e278f31ca17210c2cabb0cb758d6ba5bc257b21";
+  public static final String GIT_DATE = "2026-02-16 21:36:58 EST";
+  public static final String GIT_BRANCH = "vision";
+  public static final String BUILD_DATE = "2026-02-17 18:27:02 EST";
+  public static final long BUILD_UNIX_TIME = 1771370822950L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
