886da7bd6417ce40818ec324168a4b197d9cc17e
FRC6366RamRodz <atrmorphis@gmail.com>
2026-02-16 19:17:19 -0500

    added drive :)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,16 +6,30 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.driveTrain.CommandSwerveDrivetrain;
+import frc.robot.subsystems.driveTrain.Telemetry;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.indexer.IndexerIOTalonFX;
 import frc.robot.subsystems.kicker.Kicker;
 import frc.robot.subsystems.kicker.KickerIOTalonFX;
 import frc.robot.subsystems.shooter.Shooter;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -34,8 +48,28 @@ public class RobotContainer {
   private ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController = new CommandXboxController(
+  private final CommandXboxController driverController = new CommandXboxController(
       OperatorConstants.kDriverControllerPort);
+
+        private double MaxSpeed =
+      1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double MaxAngularRate =
+      RotationsPerSecond.of(0.75)
+          .in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final SwerveRequest.FieldCentric drive =
+      new SwerveRequest.FieldCentric()
+          .withDeadband(MaxSpeed * 0.1)
+          .withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+          .withDriveRequestType(
+              DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+  private final Telemetry logger = new Telemetry(MaxSpeed);
+
+  public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -64,14 +98,59 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+
+        // Note that X is defined as forward according to WPILib convention,
+    // and Y is defined as to the left according to WPILib convention.
+    drivetrain.setDefaultCommand(
+        // Drivetrain will execute this command periodically
+        drivetrain.applyRequest(
+            () ->
+                drive
+                    .withVelocityX(
+                        -driverController.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(
+                        -driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(
+                        -driverController.getRightX()
+                            * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            ));
+
+
+        // Idle while the robot is disabled. This ensures the configured
+    // neutral mode is applied to the drive motors while disabled.
+    final var idle = new SwerveRequest.Idle();
+    RobotModeTriggers.disabled()
+        .whileTrue(drivetrain.applyRequest(() -> idle).ignoringDisable(true));
+
+    driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    driverController
+        .b()
+        .whileTrue(
+            drivetrain.applyRequest(
+                () ->
+                    point.withModuleDirection(
+                        new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))));
+
+    // Run SysId routines when holding back/start and X/Y.
+    // Note that each routine should be run exactly once in a single log.
+    driverController.back().and(driverController.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    driverController.back().and(driverController.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    driverController.start().and(driverController.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    driverController.start().and(driverController.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+    // Reset the field-centric heading on left bumper press.
+    driverController.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+
+    drivetrain.registerTelemetry(logger::telemeterize);
+
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is
     // pressed,
     // cancelling on release.
-    m_driverController.y().whileTrue(Commands.runOnce(() -> shooter.shooterDecrements()));
-    m_driverController.x().whileTrue(Commands.runOnce(() -> shooter.shooterIncrements()));
-    m_driverController.b().whileTrue(Commands.parallel(
+    driverController.y().whileTrue(Commands.runOnce(() -> shooter.shooterDecrements()));
+    driverController.x().whileTrue(Commands.runOnce(() -> shooter.shooterIncrements()));
+    driverController.b().whileTrue(Commands.parallel(
         Commands.runOnce(() -> shooter.stop()),
         Commands.runOnce(() -> kicker.turnOffKicker())
     // Commands.runOnce(() -> indexer.stop()
@@ -82,11 +161,11 @@ public class RobotContainer {
     // m_driverController.rightTrigger().whileTrue(Commands.runOnce(() -> kicker.kickIncrements()));
 
     // New simpified kicker commands
-    m_driverController.leftTrigger().whileTrue(kicker.kickDecrement());
-    m_driverController.rightTrigger().whileTrue(kicker.kickIncrement());
+    driverController.leftTrigger().whileTrue(kicker.kickDecrement());
+    driverController.rightTrigger().whileTrue(kicker.kickIncrement());
 
-    m_driverController.leftBumper().whileTrue(Commands.runOnce(() -> indexer.indexDecrements()));
-    m_driverController.rightBumper().whileTrue(Commands.runOnce(() -> indexer.indexIncrements()));
+    driverController.leftBumper().whileTrue(Commands.runOnce(() -> indexer.indexDecrements()));
+    driverController.rightBumper().whileTrue(Commands.runOnce(() -> indexer.indexIncrements()));
 
   }
 
