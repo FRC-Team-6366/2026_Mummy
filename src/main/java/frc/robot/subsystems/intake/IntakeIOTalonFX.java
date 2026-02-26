6225c0f7cf79faa0835bea4a39f029af3a2644b8
FRC6366RamRodz <atrmorphis@gmail.com>
2026-02-26 18:28:03 -0500

    added Intake

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+
+public class IntakeIOTalonFX implements IntakeIO{
+    private TalonFX intakeMotorPivot;
+    TalonFXConfiguration iMPcfg;
+    StatusSignal<Voltage> intakePivotVolts;
+    StatusSignal<Angle> intakePivotPosition;
+    StatusSignal<AngularVelocity> intakePivotRps;
+    StatusSignal<Current> intakePivotCurrent;
+    StatusSignal<Current> intakePivotSupplyCurrent;
+
+    private TalonFX intakeMotorRollers;
+    TalonFXConfiguration iMRcfg;
+    StatusSignal<Voltage> intakeRollersVolts;
+    StatusSignal<Angle> intakeRollersPosition;
+    StatusSignal<AngularVelocity> intakeRollersRps;
+    StatusSignal<Current> intakeRollersCurrent;
+    StatusSignal<Current> intakeRollersSupplyCurrent;
+
+    MotionMagicVoltage positionVoltageRequest;
+    private VoltageOut voltageRequest;
+
+    
+
+IntakeIOTalonFX(){
+    intakeMotorRollers = new TalonFX(19);
+    iMRcfg = new TalonFXConfiguration();
+
+
+    intakeMotorPivot = new TalonFX(20);
+
+
+}
+
+}
