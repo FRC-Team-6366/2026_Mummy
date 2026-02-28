ef5a31126167fa7be42322a4134fc683ca846875
FRC6366RamRodz <atrmorphis@gmail.com>
2026-02-28 13:38:33 -0500

    Formatted it all

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -9,81 +9,84 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Intake extends SubsystemBase{
+public class Intake extends SubsystemBase {
 
     double angle = 0;
     double power = 0;
     IntakeIO intakeIO;
     IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 
-        public Intake(IntakeIO io) {
+    public Intake(IntakeIO io) {
         this.intakeIO = io;
     }
 
-        public Command intakeRunRollers(){
-                return this.runOnce(
-            () -> {
-                // Set power to full (1)
-                this.power = 1;
-
-                // Use power to start the IntakeIO Hardware motor
-                this.intakeIO.rollersRunVolts(this.power);
-            }
-        );
-    }
-
-    public Command intakeStopRollers(){
-                    return this.runOnce(
-            () -> {
-                this.intakeIO.rollersStop();
-            }
-        ); 
-    }
-    
-
-    /** For testing purposes of incrementing the position of the intake up
-     * @return Command to increment or increase the intakes pivot position. 
-     */
-        public Command intakePivotIncrements(){
+    public Command intakeRunRollers() {
         return this.runOnce(
-         () ->
-         { 
-            this.angle += 1;
-            this.angle = MathUtil.clamp(this.angle, 0.0, 45.0);
-            this.intakeIO.intakePivotToAngle(this.angle);
-         });
+                () -> {
+                    // Set power to full (1)
+                    this.power = 1;
+
+                    // Use power to start the IntakeIO Hardware motor
+                    this.intakeIO.rollersRunVolts(this.power);
+                });
     }
 
-        /** For testing purposes of decrementing the position of the intake down.
-     * @return Command to decrement or decrease the intakes pivot position. 
-     */
-        public Command intakePivotDecrements(){
+    public Command intakeStopRollers() {
         return this.runOnce(
-         () ->
-         { 
-            this.angle -= 1;
-         this.angle = MathUtil.clamp(this.angle, 0.0, 45.0);
-            this.intakeIO.intakePivotToAngle(this.angle);
-         });
+                () -> {
+                    this.intakeIO.rollersStop();
+                });
     }
-    
+
     /**
-     * Sets intake pivot motor to a certain angle based on what we put in to the double
+     * For testing purposes of incrementing the position of the intake up
+     * 
+     * @return Command to increment or increase the intakes pivot position.
+     */
+    public Command intakePivotIncrements() {
+        return this.runOnce(
+                () -> {
+                    this.angle += 1;
+                    this.angle = MathUtil.clamp(this.angle, 0.0, 45.0);
+                    this.intakeIO.intakePivotToAngle(this.angle);
+                });
+    }
+
+    /**
+     * For testing purposes of decrementing the position of the intake down.
+     * 
+     * @return Command to decrement or decrease the intakes pivot position.
+     */
+    public Command intakePivotDecrements() {
+        return this.runOnce(
+                () -> {
+                    this.angle -= 1;
+                    this.angle = MathUtil.clamp(this.angle, 0.0, 45.0);
+                    this.intakeIO.intakePivotToAngle(this.angle);
+                });
+    }
+
+    /**
+     * Sets intake pivot motor to a certain angle based on what we put in to the
+     * double
+     * 
      * @param angle Angle in degrees
      * @return Command to set the Intake Pivot Motor at angle for shooting
      */
     public Command intakePivotToAngle(double angle) {
         return this.runOnce(
-            () -> {
-                this.angle = angle;
-                this.intakeIO.intakePivotToAngle(this.angle);
-            });
+                () -> {
+                    this.angle = angle;
+                    this.intakeIO.intakePivotToAngle(this.angle);
+                });
     }
 
-      /**
+    /**
      * Retracts the Intake Pivot Motor to its starting position
      * <p>
-     * <b>NOTE: Start the robot with the Intake Pivot Motor in the fully retracted position!</b>
+     * <b>NOTE: Start the robot with the Intake Pivot Motor in the fully retracted
+     * position!</b>
+     * 
      * @return Command to set Intake Pivot Motor to starting position
      */
     public Command intakePivotAngleRetract() {
@@ -92,25 +95,31 @@ public class Intake extends SubsystemBase{
     }
 
     /**
-     * Sets intake pivot motor to a certain angle based on what we put in to the double
+     * Sets intake pivot motor to a certain angle based on what we put in to the
+     * double
      * <p>
-     * <b>NOTE: Start the robot with the Intake Pivot Motor in the fully retracted position!</b>
+     * <b>NOTE: Start the robot with the Intake Pivot Motor in the fully retracted
+     * position!</b>
+     * 
      * @return Command to set Intake Pivot Motor for close shooting
      */
-    public Command intakePivotAngleExtend(){
+    public Command intakePivotAngleExtend() {
         return this.intakePivotToAngle(Constants.IntakeConstants.intakePivotOutAngle);
     }
 
     /**
-     * Returns whether the Intake Pivot Motor is at its set point distance, given a percent of tolerence
+     * Returns whether the Intake Pivot Motor is at its set point distance, given a
+     * percent of tolerence
      * specified in the IntakeIO hardware class
-     * @return BooleanSupplier: True Intake Pivot Motor is at its setpoint, false otherwise
+     * 
+     * @return BooleanSupplier: True Intake Pivot Motor is at its setpoint, false
+     *         otherwise
      */
     public BooleanSupplier intakePivotAtPositionSetpoint() {
         return () -> this.intakeIO.intakeAtPositionSetpoint();
     }
 
-        @Override
+    @Override
     public void periodic() {
         // This method will be called once per scheduler run
 
--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -3,18 +3,24 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public interface IntakeIO {
-        /**
-     * Gets the current number of rotations of the intake pivot motor from its starting
+    /**
+     * Gets the current number of rotations of the intake pivot motor from its
+     * starting
      * position
      * <p>
-     * <b>NOTE: Move the hood to fully restracted position before turning on the robot!</b>
-     * @return Rotation2d object representing the number of rotations of the intake pivot motor
+     * <b>NOTE: Move the hood to fully restracted position before turning on the
+     * robot!</b>
+     * 
+     * @return Rotation2d object representing the number of rotations of the intake
+     *         pivot motor
      */
     public Rotation2d getRotations();
 
-        /**
-     * Sets the intake pivot motor to the position (number of rotations from start) based on
+    /**
+     * Sets the intake pivot motor to the position (number of rotations from start)
+     * based on
      * provided angle.
+     * 
      * @param angle Angle in degrees. Valid from 0 to 45 degrees max
      */
     public void intakePivotToAngle(double angle);
@@ -23,18 +29,23 @@ public interface IntakeIO {
      * Sets the intake pivot motor to move to a certian positon, otherwise known as
      * number of rotations from the intake pivot motor's starting position when the
      * robot was first turned on.
+     * 
      * @param position Number of rotations
      */
     public void intakePivotToPosition(double position);
 
     /**
-     * Gets the difference between the intake pivot motor's position (number of rotations) from its current setpoint
+     * Gets the difference between the intake pivot motor's position (number of
+     * rotations) from its current setpoint
+     * 
      * @return Error amount
      */
     public double getIntakePivotPositionError();
 
     /**
-     * Returns whether the hood is at its set point distance, given a percent of tolerence.
+     * Returns whether the hood is at its set point distance, given a percent of
+     * tolerence.
+     * 
      * @return True if hood is at setpoint, false otherwise
      */
     public boolean intakeAtPositionSetpoint();
@@ -44,5 +55,5 @@ public interface IntakeIO {
     public void rollersStop();
 
     public void updateInputs(IntakeIOInputs inputs);
-    
+
 }
