0a67cd9d2468ee1593a78420fb1cae6f6abc5b5b
Will E <wre136@hotmail.com>
2026-03-06 13:56:44 -0500

    Removed Example Subsystem

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 170;
-  public static final String GIT_SHA = "90894d702d4dafbe11062a306bbd7054601c1687";
-  public static final String GIT_DATE = "2026-03-06 12:49:53 EST";
+  public static final int GIT_REVISION = 171;
+  public static final String GIT_SHA = "7b4f56c27c75f5f02fcbb1698d685af70f1efce2";
+  public static final String GIT_DATE = "2026-03-06 13:14:41 EST";
   public static final String GIT_BRANCH = "event-2026-03-05-North-Charleston";
-  public static final String BUILD_DATE = "2026-03-06 13:00:54 EST";
-  public static final long BUILD_UNIX_TIME = 1772820054448L;
+  public static final String BUILD_DATE = "2026-03-06 13:45:09 EST";
+  public static final long BUILD_UNIX_TIME = 1772822709197L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
