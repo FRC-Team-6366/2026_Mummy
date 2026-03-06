f223323fe9d3e3c78a15512c0288cb3451b1266a
FRC6366RamRodz <atrmorphis@gmail.com>
2026-03-06 14:17:47 -0500

    Update at 'Fri Mar 06 14:17:47 EST 2026'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 172;
-  public static final String GIT_SHA = "7ada3cc2ef4e65b697aef58f9d750954121fc1ea";
-  public static final String GIT_DATE = "2026-03-06 14:11:16 EST";
+  public static final int GIT_REVISION = 173;
+  public static final String GIT_SHA = "6cc033987be215945d3501bb64456bb1489c8e85";
+  public static final String GIT_DATE = "2026-03-06 14:12:46 EST";
   public static final String GIT_BRANCH = "event-2026-03-05-North-Charleston";
-  public static final String BUILD_DATE = "2026-03-06 14:11:17 EST";
-  public static final long BUILD_UNIX_TIME = 1772824277767L;
+  public static final String BUILD_DATE = "2026-03-06 14:12:47 EST";
+  public static final long BUILD_UNIX_TIME = 1772824367173L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
