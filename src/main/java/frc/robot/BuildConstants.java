6c3a41e2b6201a328adc7c7bcffd3034bbb4d5bf
FRC6366RamRodz <atrmorphis@gmail.com>
2026-03-05 20:30:17 -0500

    Set a brake mode

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 145;
-  public static final String GIT_SHA = "0bfea123b97c74b5a8b81455b16b65292c61da0c";
-  public static final String GIT_DATE = "2026-03-05 20:25:34 EST";
+  public static final int GIT_REVISION = 146;
+  public static final String GIT_SHA = "0afa2f248a242993aac10535c77d29a0bec160bd";
+  public static final String GIT_DATE = "2026-03-05 20:28:50 EST";
   public static final String GIT_BRANCH = "event-2026-03-05-North-Charleston";
-  public static final String BUILD_DATE = "2026-03-05 20:25:34 EST";
-  public static final long BUILD_UNIX_TIME = 1772760334836L;
+  public static final String BUILD_DATE = "2026-03-05 20:28:51 EST";
+  public static final long BUILD_UNIX_TIME = 1772760531298L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
