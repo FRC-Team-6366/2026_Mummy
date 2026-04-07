a6fecc10595c11766a2c712172b21e5f6ab32fc4
FRC6366RamRodz <atrmorphis@gmail.com>
2026-04-07 17:28:27 -0400

    added lft bumper pump

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 382;
-  public static final String GIT_SHA = "c9a7d47bc03426d766f2658c6b8f1e1e2653fe87";
-  public static final String GIT_DATE = "2026-04-04 13:13:35 EDT";
+  public static final int GIT_REVISION = 383;
+  public static final String GIT_SHA = "51c68022890e3e6f5eaed1ff6eac99716e634639";
+  public static final String GIT_DATE = "2026-04-06 11:53:48 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-04 14:30:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1775327447869L;
+  public static final String BUILD_DATE = "2026-04-07 17:27:27 EDT";
+  public static final long BUILD_UNIX_TIME = 1775597247976L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
