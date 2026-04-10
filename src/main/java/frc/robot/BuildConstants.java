dd6c180d8194a83f9fafa68d636c211909c38edd
FRC6366RamRodz <atrmorphis@gmail.com>
2026-04-10 10:59:12 -0400

    trying to fix a

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 403;
-  public static final String GIT_SHA = "0c81ee315f19d09d59cd2a8c1e0182099c625519";
-  public static final String GIT_DATE = "2026-04-10 08:05:29 EDT";
+  public static final int GIT_REVISION = 404;
+  public static final String GIT_SHA = "4784bd76f82ef2a7faabb36a603f82f95596e91a";
+  public static final String GIT_DATE = "2026-04-10 09:20:04 EDT";
   public static final String GIT_BRANCH = "event-2026-03-09-AndersonDistricts";
-  public static final String BUILD_DATE = "2026-04-10 08:05:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1775822732915L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-04-10 10:45:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1775832346277L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
