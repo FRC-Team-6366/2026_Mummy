0c81ee315f19d09d59cd2a8c1e0182099c625519
FRC6366RamRodz <atrmorphis@gmail.com>
2026-04-10 08:05:29 -0400

    Update at 'Fri Apr 10 08:05:27 EDT 2026'

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
@@ -215,9 +215,9 @@ public class ShooterIOTalonFX implements ShooterIO {
     double rotationsPerSecond = feetPerSecond / ((4.0 / 12.0) * Math.PI);
     double rpsToUse = MathUtil.clamp(rotationsPerSecond, shooterMinVelocityRPS, shooterMaxVelocityRPS);
 
-    this.rightLeadShooterMotor.setControl(velocityVoltageRequest.withVelocity(rpsToUse* 0.925));
+    this.rightLeadShooterMotor.setControl(velocityVoltageRequest.withVelocity(rpsToUse* 0.94));
     this.rightFollowerShooterMotor.setControl(this.rightFollower);
-    this.leftLeadShooterMotor.setControl(velocityVoltageRequest.withVelocity(rpsToUse* 0.925));
+    this.leftLeadShooterMotor.setControl(velocityVoltageRequest.withVelocity(rpsToUse* 0.94));
     this.leftFollowerShooterMotor.setControl(this.leftFollower);
   }
 
