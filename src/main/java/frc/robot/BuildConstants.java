d061686652275404fa55f34384ec12470c6f9259
FRC6366RamRodz <atrmorphis@gmail.com>
2026-04-21 20:21:55 -0400

    Phoenix Drive tune

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 485;
-  public static final String GIT_SHA = "7a72a1ec3ce18a5a889db663b13bb0e23d4ec9a8";
-  public static final String GIT_DATE = "2026-04-21 17:42:47 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-21 17:44:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1776807850190L;
+  public static final int GIT_REVISION = 490;
+  public static final String GIT_SHA = "5b84895b765c23ed9df71b541ad48c0eace76ece";
+  public static final String GIT_DATE = "2026-04-21 19:47:36 EDT";
+  public static final String GIT_BRANCH = "Drive";
+  public static final String BUILD_DATE = "2026-04-21 20:18:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1776817139517L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -23,13 +23,13 @@ public class TunerConstants {
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
         .withKP(100).withKI(0).withKD(0.5)
-        .withKS(0.1).withKV(1.91).withKA(0)
+        .withKS(0.1).withKV(1.91).withKA(0)//P100,I0,D.5,S.1,K1.91
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(2).withKI(0).withKD(0)
-        .withKS(0).withKV(0.124);
+        .withKP(2.17).withKI(0).withKD(0)
+        .withKS(0).withKV(0.124);//P2IDV.124
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
