53970216b77dee9f8ccf9a5894011279b6b8820e
FRC6366RamRodz <atrmorphis@gmail.com>
2026-04-29 15:51:09 -0400

    aurtos in progress

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 491;
-  public static final String GIT_SHA = "7d985570bea32587402111b44580cc136913ca05";
-  public static final String GIT_DATE = "2026-04-23 17:06:06 EDT";
-  public static final String GIT_BRANCH = "testing-Current";
-  public static final String BUILD_DATE = "2026-04-23 18:55:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1776984929771L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 496;
+  public static final String GIT_SHA = "b1a91ef695efa6cb8cd9b610aea8b7a43924c735";
+  public static final String GIT_DATE = "2026-04-29 13:17:53 EDT";
+  public static final String GIT_BRANCH = "event-2-29-2026-Worlds-Houston-Texas";
+  public static final String BUILD_DATE = "2026-04-29 13:18:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1777483095125L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
