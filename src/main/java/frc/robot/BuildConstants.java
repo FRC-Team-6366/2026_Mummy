be70306a48152b4c5d79956e06d788cde360ca4d
nickdupre <nickdupr3@gmail.com>
2026-04-29 23:18:36 -0500

    updated vendor deps, added camera status to elastic

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_Mummy";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 499;
-  public static final String GIT_SHA = "348992ac2fce60b1bfdd4ce1f836bd5b73bd4909";
-  public static final String GIT_DATE = "2026-04-29 18:57:07 EDT";
+  public static final int GIT_REVISION = 501;
+  public static final String GIT_SHA = "6bd17a1803c8874bad954dbb039d2f200884529b";
+  public static final String GIT_DATE = "2026-04-29 23:26:52 EDT";
   public static final String GIT_BRANCH = "event-2-29-2026-Worlds-Houston-Texas";
-  public static final String BUILD_DATE = "2026-04-29 18:57:07 EDT";
-  public static final long BUILD_UNIX_TIME = 1777503427814L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-04-30 00:10:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1777522203961L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
